ae49777370a589484c84d4fc7dcd66d625fee21a
tubademir23 <tubademir23@gmail.com>
2019-09-17 17:46:21 +0300

    cred message

--- functions/index.js
+++ functions/index.js
@@ -258,10 +258,13 @@ app.post('/login',(req,res)=>{
     return res.json(token);
   })
   .catch(err=>{
-    console.error(err);
-    return res.status(500).json({
-      error:err.code
-    });
+    if(err.code==='auth/wrong-password'){
+      return res.status(403).json({general:'wrong credentials, please control'});
+    }else {
+      return res.status(500).json({
+        error:err.code
+      });
+    }
   })
   
 })
