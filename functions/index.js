0dab4b3dd994892c25e3a8348041e87f7e58f271
tubademir23 <tubademir23@gmail.com>
2019-09-23 11:49:38 +0300

    error db.doc

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,8 @@ const config = require('./util/config');
 const functions = require('firebase-functions');
 const FBAuth = require('./util/fbAuth');
 const app = require('express')(); 
-const db= require('./util/admin');
+const {db} = require('./util/admin');
+
 const{getAllScreams, 
     postOneScream, 
     getScream,
@@ -43,30 +44,31 @@ app.get('/user',FBAuth, getAuthenticatedUser);
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
 
-exports.createNotificationOnLike=functions.region('europe-west1')
-.firestore.document('/likes/{id}')
-.onCreate((snapshot)=>{
-    db.doc(`/screams/${snapshot.data().screamId}`).get()
-    .then(doc=>{
-        if(doc.exists){
-            return db.doc(`/notifications/${snapshot.id}`).set({
-                createdAt:new Date().toISOString(),
-                recipient:doc.data().userHandle,
-                sender:snapshot.data().userHandle,
-                type:'like',
-                read:false,
-                screamId: doc.id
-            })
+exports.createNotificationOnLike = functions
+  .region('europe-west1')
+  .firestore.document('likes/{id}')
+  .onCreate((snapshot) => {
+    //   console.log('create notifications');
+    return db
+      .doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (
+          doc.exists &&
+          doc.data().userHandle !== snapshot.data().userHandle
+        ) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'like',
+            read: false,
+            screamId: doc.id
+          });
         }
-    })
-    .then(()=>{
-        return;
-    })
-    .catch(err=>{
-        console.error(err);
-        return ;
-    })
-});
+      })
+      .catch((err) => console.error(err));
+  });
 exports.deleteNotificationOnUnlike=functions
 .region('europe-west1')
 .firestore.document('/likes/{id}')
